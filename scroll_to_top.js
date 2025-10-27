e1a4712e885e41fccae22276794a16ffcbb1f292
aaditimenon <saaditimenon@gmail.com>
2025-10-27 18:52:34 +0530

    Add scroll-to-top button

---
+++ scroll_to_top.js
@@ -0,0 +1,19 @@
+let mybutton = document.getElementById("scrollupBtn");
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0; // Safari
+  document.documentElement.scrollTop = 0; //Chrome, Firefox, IE and Opera
+}
